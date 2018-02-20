015f6afee0b6c44ca84cbd56a479d11ff1fa8187
Dongwan <dkim125@my.centennialcollege.ca>
2018-02-19 23:35:46 -0500

    first commit

---
+++ Scripts/Core/game.ts
@@ -0,0 +1,12 @@
+/*
+    Name : Dongwan Kim
+    Version : v1.0
+    Last_modification : Feb 19, 2018
+    Description : Created a game.ts
+*/
+
+(function(){
+    //Game Variables
+    let canvas = document.getElementById("canvas");
+    
+})();
