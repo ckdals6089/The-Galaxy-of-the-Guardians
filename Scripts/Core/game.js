eb17a46c0266dd1423e2966e9a724e3aeed59852
Dongwan <dkim125@my.centennialcollege.ca>
2018-02-20 00:02:42 -0500

    added typescript, npm support and createjs supports

---
+++ Scripts/Core/game.js
@@ -0,0 +1,11 @@
+/*
+    Name : Dongwan Kim
+    Version : v1.0
+    Last_modification : Feb 19, 2018
+    Description : Created a game.ts
+*/
+(function () {
+    //Game Variables
+    var canvas = document.getElementById("canvas");
+})();
+//# sourceMappingURL=game.js.map
