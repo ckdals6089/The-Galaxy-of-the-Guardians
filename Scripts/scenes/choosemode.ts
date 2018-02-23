7bf839720bdc4345be762d0630ea2a3637838ad9
Joy-Shin <joy.shin.2213@gmail.com>
2018-02-22 19:51:08 -0500

    Added Button Back on the choosemode.ts

--- Scripts/scenes/choosemode.ts
+++ Scripts/scenes/choosemode.ts
@@ -1,8 +1,8 @@
 /*
-    Name : Dongwan Kim
-    Version : v1.0
-    Last_modification : Feb 20, 2018
-    Description : Created chooseModeScene
+    Name : Dongwan Kim, Jowon Shin
+    Version : v1.1
+    Last_modification : Feb 22, 2018
+    Description : Created Back Button
 */
 
 module scenes{
@@ -10,24 +10,33 @@ module scenes{
         //PRIVATE VARIABLES
         private _btnNormal: objects.Button;
         private _btnHell: objects.Button;
+        private _btnBack: objects.Button;
         //PUBLIC PROPERTIES
 
         //CONSTRUCTOR
         constructor(assetManager:createjs.LoadQueue){
-            super(assetManager);
+            super(assetManager)
             this.Start();
         }
+
         //PRIVATE METHODS
         private _btnNormalClick():void{
             objects.Game.currentScene = config.Scene.PLAY;
         }
+
         private _btnHellClick():void{
             objects.Game.currentScene = config.Scene.PLAY;
         }
+
+        private _btnBackClick():void {
+            objects.Game.currentScene = config.Scene.OPENING;
+        }
+
         //PUBLIC METHODS
         public Start():void{
             this._btnNormal = new objects.Button(this.assetManager, "btnNormal", 200, 340);
             this._btnHell = new objects.Button(this.assetManager, "btnHell", 440, 340);
+            this._btnBack = new objects.Button(this.assetManager, "btnBack", 500, 100);
             this.Main();
             console.log("start");
         }
@@ -38,10 +47,11 @@ module scenes{
         public Main():void{
             this.addChild(this._btnNormal);
             this.addChild(this._btnHell);
-
+            this.addChild(this._btnBack);
     
             this._btnNormal.on("click", this._btnNormalClick);
             this._btnHell.on("click",this._btnHellClick);
+            this._btnBack.on("Click", this._btnBackClick);
         }
         // public Init():void{
 
