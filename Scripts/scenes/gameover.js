7dcb815d483e8d1a46e30ef2cb10be73beab84af
Changmin <cshin4@my.centennialcollege.ca>
2018-03-22 18:35:39 -0400

    changed label of gameover scenes

--- Scripts/scenes/gameover.js
+++ Scripts/scenes/gameover.js
@@ -33,7 +33,7 @@ var scenes;
         GameOverScene.prototype.Start = function () {
             this._btnPlayAgain = new objects.Button("btnPlayAgain", 320, 360);
             this._lblGameOver = new objects.Label("Game Over", "40px", "SpaceComic", "#FF0000", 320, 240, true);
-            this._lblScore = new objects.Label("High Score: ", "40px", "SpaceComic", "#FF0000", 140, 120, false);
+            this._lblScore = new objects.Label("High Score: ", "40px", "SpaceComic", "#FF0000", 120, 120, false);
             this._scoreboard = new managers.ScoreBoard;
             this.Main();
             console.log("game over");
